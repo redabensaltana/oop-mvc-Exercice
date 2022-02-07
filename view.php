1c9929f3121587b9788a87fb0483b374122bad17
redakeyboardboy <redakeyboardboy@gmail.com>
2022-02-07 17:00:26 +0100

    look enhance

--- view.php
+++ view.php
@@ -4,13 +4,16 @@
     </title>
   </head>
   <body>
-   <h1>Players</h1>
-
+   <h1 style="font-family:Roboto;">Players</h1>
+      
     <?php foreach($result as $player): ?>
         <hr>
-        <h2><?= $player['number'] ?></h2>
-        <p><?= $player['name'] ?></p>
-        <p><?= $player['salary'] ?></p>
+
+        <div style="font-family:Roboto;display:flex;justify-content:space-around;align-items:center;border:2px solid black;border-radius: 20px;width:400px">
+          <h3><?= $player['number'] ?></h3>
+          <p><?= $player['name'] ?></p>
+          <p><?= $player['salary'] ?>$</p>
+        </div>
         
     <?php endforeach; ?>    
   </body>
